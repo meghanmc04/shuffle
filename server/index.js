5cf24dedcd8e09407234b336eb50d5dbf9e6ff00
Meghan McFarland <meghanmc04@mac.com>
2021-04-17 14:42:55 -0400

    fix(server) fixes error in chalk import

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
-const { magentaBright, red } = require("chalk");
+const { greenBright, red } = require("chalk");
 
 const app = express();
 const port = process.env.PORT || 8080;
