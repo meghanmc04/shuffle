6d89c9284fa80e93d001f22d2eafd31afb115eb0
Meghan McFarland <meghanmc04@mac.com>
2021-04-17 14:05:16 -0400

    feat(server/routes) adds 404 error for unknown routes

--- server/api/index.js
+++ server/api/index.js
@@ -4,4 +4,10 @@ router.use("/users", require("./users"));
 router.use("/series", require("./series"));
 router.use("/movies", require("./movies"));
 
+router.use((req, res, next) => {
+  const err = new Error("api route not found!");
+  err.status = 404;
+  next(err);
+});
+
 module.exports = router;
