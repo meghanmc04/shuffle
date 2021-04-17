67a48178d3d6eddac20a60f804fd42fa3f3625bb
Meghan McFarland <meghanmc04@mac.com>
2021-04-17 14:09:07 -0400

    feat(server) adds 500 error handling + listens on port 8080

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "../public/")));
@@ -14,3 +15,12 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "internal server error");
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
